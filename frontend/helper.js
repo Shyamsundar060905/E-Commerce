21e5ffd8df60db3c57eadc61b34de56eeb59cf8a
Shyamsundar Paramasivam <shyamsundar.paramasivam@gmail.com>
2026-03-15 17:20:43 +0530

    Changed backend URL

--- frontend/helper.js
+++ frontend/helper.js
@@ -1,4 +1,5 @@
-const URL = "http://localhost:3000";
+const URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 export default URL;
 export const TAX_RATE = 0.18;
 export const FREE_SHIPPING_THRESHOLD = 2000;
