8acedbe7bfb6d9f1e51ee1ea87898831bc84761d
Shyamsundar Paramasivam <shyamsundar.paramasivam@gmail.com>
2026-03-20 12:08:46 +0530

    Changed login cookie

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -67,11 +67,10 @@ export const login = catchAsync(async (req, res, next) => {
   const token = signToken(user._id);
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: false,
-    sameSite: "lax", // ✅ NOT "none"
+    secure: true, // ✅ REQUIRED in production
+    sameSite: "none", // ✅ REQUIRED for cross-site
     maxAge: 24 * 60 * 60 * 1000,
   });
-
   res.status(200).json({
     status: "success",
     data: {
